430a55647b24082b2e965b4fea6b82ff273da694
mvecerin <mvecerin@protonmail.com>
2021-05-04 10:48:07 +0200

    deployment changes

--- server.ts
+++ server.ts
@@ -38,9 +38,9 @@ app.use(cors());
 // Routers
 app.use("/api", indexRouter);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname + "..", "client", "build")));
+  app.use(express.static(path.join(__dirname + "/..", "client", "build")));
   app.get("*", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname + "..", "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/..", "client", "build", "index.html"));
   });
 }
 
