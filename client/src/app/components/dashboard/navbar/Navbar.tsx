d8b248c7d0c8c872a7b7cb9753f56bf8e84a0f32
mvecerin <mvecerin@protonmail.com>
2021-05-31 11:55:50 +0200

    navbar menu fix

--- client/src/app/components/dashboard/navbar/Navbar.tsx
+++ client/src/app/components/dashboard/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useRouteMatch } from "react-router-dom";
-import { RouteParams } from "../../../../utils/interfaces";
+import { IGroup, RouteParams } from "../../../../utils/interfaces";
 import { AddInviteDialog } from "./AddInviteDialog";
 import { IconWithText } from "../../other/IconWithText";
 import { GroupNavBar } from "./GroupNavBar";
@@ -17,17 +17,7 @@ export const Navbar = () => {
   );
   return (
     <>
-      {group?._id && (
-        <>
-          <AddInviteDialog
-            groupId={group._id}
-            inviteToken={group.inviteToken!}
-          />
-          <DeleteGroupDialog groupId={group._id} groupTitle={group.title} />
-          <LeaveGroupDialog groupId={group._id} groupTitle={group.title} />
-          <EditTitleDialog groupId={group._id} groupTitle={group.title} />
-        </>
-      )}
+      {group?._id && <Dialogs group={group} />}
       <header className="d-flex sticky-top flex-md-nowrap align-items-center bg-dark">
         <Link
           className="navbar-brand bg-primary col-md-3 col-xl-2 ps-3 fs-3 me-0 link-light"
@@ -38,7 +28,7 @@ export const Navbar = () => {
         <div className="w-100 d-flex align-items-center my-1">
           {group?._id && <GroupNavBar group={group} />}
           <button
-            className="navbar-toggler d-md-none collapsed bi-list btn btn-dark me-2"
+            className="navbar-toggler d-md-none collapsed bi-list btn btn-dark me-2 ms-auto"
             data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu"
             aria-label="Toggle navigation"
@@ -48,3 +38,15 @@ export const Navbar = () => {
     </>
   );
 };
+
+interface Props {
+  group: IGroup;
+}
+const Dialogs = ({ group }: Props) => (
+  <>
+    <AddInviteDialog groupId={group._id!} inviteToken={group.inviteToken!} />
+    <DeleteGroupDialog groupId={group._id!} groupTitle={group.title} />
+    <LeaveGroupDialog groupId={group._id!} groupTitle={group.title} />
+    <EditTitleDialog groupId={group._id!} groupTitle={group.title} />
+  </>
+);
